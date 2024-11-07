54241a544eb725603eb2221e34aba71bfcfe3390
Aung Thu ( Arun Katwal ) <60294099+drunisa007@users.noreply.github.com>
2024-11-07 16:57:14 +0700

    Create fidotestone.rb

---
+++ fidotestone.rb
@@ -0,0 +1,9 @@
+cask "fidotestone" do
+  version "1.0.0+2"
+  sha256 "ab4efc63b2a145748a9fa46d3267f2292bc4c8384a1524d904e835120f40bb9b"
+  url "https://github.com/drunisa007/updater_xml/releases/download/prod/fidotestone.zip"
+  name "Fidotestone"
+  homepage "https://www.aungthu.com"
+  
+  app "fidotestone.app"
+end
